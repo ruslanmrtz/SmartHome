7cf5b5317943b6f2adc45db047b0daf3f53cb650
Ruslan <ruslanmrtzn@yandex.ru>
2025-10-01 09:36:01 +0500

    bagfix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 
+from weather import get_weather
 # from whisper import get_text_from_audio
 # from text_classification import classify_text
 
@@ -16,27 +17,37 @@ app = FastAPI(
 async def read_index():
     return FileResponse("index_new.html")
 
-#
-# @app.post("/transcribe/", summary=" Распознавание речи")
-# async def transcribe_audio(file: UploadFile = File(...)):
-#     """
-#     Принимает аудиофайл и возвращает текст.
-#     Поддерживаемые форматы: любые.
-#     """
-#     # Проверка, что файл передан
-#     if not file:
-#         raise HTTPException(status_code=400, detail="Файл не передан")
-#
-#     text = await get_text_from_audio(file)
-#
-#     classification = await classify_text(text)
-#
-#     # Опционально: можно вернуть имя файла и размер
-#     return JSONResponse({
-#         "filename": file.filename,
-#         "size_bytes": file.size,
-#         "transcribed_text": classification
-#     })
+
+@app.get("/weather")
+async def read_index():
+    
+    weather = get_weather()
+    weather = {'conditions': 'Солнечно',
+                'temp': '+18'}
+    
+    return weather
+
+
+@app.post("/transcribe/", summary=" Распознавание речи")
+async def transcribe_audio(file: UploadFile = File(...)):
+    """
+    Принимает аудиофайл и возвращает текст.
+    Поддерживаемые форматы: любые.
+    """
+    # Проверка, что файл передан
+    if not file:
+        raise HTTPException(status_code=400, detail="Файл не передан")
+
+    text = await get_text_from_audio(file)
+
+    classification = await classify_text(text)
+
+    # Опционально: можно вернуть имя файла и размер
+    return JSONResponse({
+        "filename": file.filename,
+        "size_bytes": file.size,
+        "transcribed_text": classification
+    })
 
 
 # Для запуска через uvicorn (если файл называется main.py)
